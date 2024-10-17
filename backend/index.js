a1cbe8911061e38ef25b4cb5c458507a0963e4c7
Vicky8180 <vyadav99x1@gmail.com>
2024-10-18 00:37:28 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const db = require("./Config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+dotenv.config();
+// db();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+//   "https://share-story-x.vercel.app",
+  "http://another-origin.com",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS,PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// app.use("/api", require("./Router/routes"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port no ${PORT}`);
+});
